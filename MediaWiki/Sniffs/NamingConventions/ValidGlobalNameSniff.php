29ad1b875b1ff60667a54d756aee75e95dd9698b
Antoine Musso <hashar@free.fr>
2012-01-19 12:43:41 +0100

    verify global variable names

---
+++ MediaWiki/Sniffs/NamingConventions/ValidGlobalNameSniff.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Verify MediaWiki global variable naming convention.
+ * A global name must be prefixed with 'wg'.
+ */
+class MediaWiki_Sniffs_NamingConventions_ValidGlobalNameSniff implements PHP_CodeSniffer_Sniff {
+
+	public function register() {
+		return array( T_GLOBAL );
+	}
+
+	public function process( PHP_CodeSniffer_File $phpcsFile, $stackPtr ) {
+		$tokens = $phpcsFile->getTokens();
+		$token = $tokens[$stackPtr];
+
+		if( $token['code'] === T_GLOBAL ) {
+			$errorIssued = false;
+
+			$nameIndex  = $phpcsFile->findNext( T_VARIABLE, $stackPtr + 1 );
+			$globalName = $tokens[$nameIndex]['content'];
+
+
+			// skip '$' and forge a valid global variable name
+			$expected = '$wg' . ucfirst(substr( $globalName, 1 ));
+
+
+			// Verify global is prefixed with wg
+			if( strpos($globalName, '$wg' ) !== 0 ) {
+
+				$error = 'Global variable "%s" is lacking \'wg\' prefix. Should be "%s".';
+				$type = 'wgPrefix';
+				$data = array( $globalName, $expected );
+				$phpcsFile->addError( $error, $stackPtr, $type, $data );
+
+				$errorIssued = true;
+			}
+
+			if( !$errorIssued ) { // no need to warn twice.
+				// Verify global is probably CamelCase
+				$val = ord( substr( $globalName, 3, 1 ) );
+				print "$globalName -> $val\n";
+				if( !($val >= 65 && $val <= 90) ) {
+
+					$error = 'Global variable "%s" should use CamelCase. Should be "%s"';
+					$type = 'CamelCase';
+					$data = array( $globalName, $expected );
+					$phpcsFile->addError( $error, $stackPtr, $type, $data );
+				}
+			}
+		}
+
+	}//end process()
+
+}
+
