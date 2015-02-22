7a967351c8ce12a5f693b3df1ba35db12ffef6dd
Sumit Asthana <asthana.sumit23@gmail.com>
2015-02-22 23:10:17 +0530

    Sniff to detect text before first opening php tag
    Detects additional characters before first php open tag. Ignores subsequent php
    open tags which can be preceeded by characters as in templates.
    
    Bug: T46875
    Change-Id: Ie9b60139a3cb9e3e585b382e16f0305b683affcc

---
+++ MediaWiki/Sniffs/ExtraCharacters/CharacterBeforePHPOpeningTagSniff.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Check to see if there's any character before php open tag <? or <?php
+ */
+class MediaWiki_Sniffs_ExtraCharacters_CharacterBeforePHPOpeningTagSniff implements PHP_CodeSniffer_Sniff {
+	public function register() {
+		return array( T_OPEN_TAG );
+	}
+	public function process( PHP_CodeSniffer_File $phpcsFile, $stackPtr ) {
+		// do nothing if opening tag is the first character
+		if( $stackPtr == 0) {
+			return;
+		}
+		$tokens = $phpcsFile->getTokens();
+		$isNotFirstOpenTag = $phpcsFile->findPrevious(
+			array( T_OPEN_TAG ),
+			( $stackPtr - 1 ),
+			null,
+			false
+		);
+		// some other character beginning file
+		if ( $isNotFirstOpenTag === false ) {
+			$error = 'Extra character found before first <?';
+			$phpcsFile->addError( $error, $stackPtr, 'Found' );
+		}
+	}
+}
---
+++ MediaWiki/Tests/files/ExtraCharacters/extra_characters_before_phpopen_tag_pass.php
@@ -0,0 +1,7 @@
+<?php
+// Valid php open tag
+function foo() {
+
+}
+?>
+ <?php //this php open tag will in any case be ignored
