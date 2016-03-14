bb0bb67d691e6eb69f12609492c31e42e3f2346d
Lethexie <xietaoecho@gmail.com>
2016-03-14 20:08:52 +0800

    Make IfElseStructureSniff can detect and fix multiple white spaces after else
    - The original version can't detect and fix multiple white spaces after else.
    
    - Now it will detect and fix multiple white spaces between else and opening brace.
    
    Bug: T129840
    Change-Id: Ie645bd1217614c828d146efac964f690ac48b6f1

--- MediaWiki/Sniffs/ControlStructures/IfElseStructureSniff.php
+++ MediaWiki/Sniffs/ControlStructures/IfElseStructureSniff.php
@@ -20,7 +20,9 @@ class MediaWiki_Sniffs_ControlStructures_IfElseStructureSniff
 	public function process( PHP_CodeSniffer_File $phpcsFile, $stackPtr ) {
 		$tokens = $phpcsFile->getTokens();
 		$prevToken = $tokens[$stackPtr - 1];
+		$nextToken = $tokens[$stackPtr + 1];
 
+		// single space expected before else and elseif structure
 		if ( $prevToken['code'] !== T_WHITESPACE
 			|| $prevToken['content'] !== " " ) {
 			$fix = $phpcsFile->addFixableWarning(
@@ -37,6 +39,31 @@ class MediaWiki_Sniffs_ControlStructures_IfElseStructureSniff
 					$phpcsFile->fixer->replaceToken( $stackPtr - 1, ' ' );
 					for ( $i = 2; $tokens[$stackPtr - $i]['code'] === T_WHITESPACE; $i++ ) {
 						$phpcsFile->fixer->replaceToken( $stackPtr - $i, '' );
+					}
+				}
+			}
+		}
+		// space after elseif structure be processed in SpaceAfterControlStructureSniff
+		if ( $tokens[$stackPtr]['code'] === T_ELSEIF ) {
+			return;
+		}
+		// single space expected after else structure
+		if ( $nextToken['code'] !== T_WHITESPACE
+			|| $nextToken['content'] !== " " ) {
+			$fix = $phpcsFile->addFixableWarning(
+				'Single space expected after "%s"',
+				$stackPtr + 1,
+				'SpaceAfterElse',
+				[ $tokens[$stackPtr]['content'] ]
+			);
+			if ( $fix === true ) {
+				if ( $nextToken['code'] === T_OPEN_CURLY_BRACKET ) {
+					$phpcsFile->fixer->addContnetAfter( $stackPtr, ' ' );
+				} else {
+					// Replace all after whitespace with a space
+					$phpcsFile->fixer->replaceToken( $stackPtr + 1, ' ' );
+					for ( $i = 2; $tokens[$stackPtr + $i]['code'] === T_WHITESPACE; $i++ ) {
+						$phpcsFile->fixer->replaceToken( $stackPtr + $i, '' );
 					}
 				}
 			}
