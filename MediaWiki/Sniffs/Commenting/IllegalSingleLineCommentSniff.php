b11a8ee5280dec1cea38a77d01bfc4b99d149cb0
Lethe <xietaoecho@gmail.com>
2016-06-24 04:29:15 -0700

    Single Line comments no multiple '*'.
    - The original version does not detect '/***** Do not comment like this ***/' mentioned
    in https://www.mediawiki.org/wiki/Manual:Coding_conventions/PHP.
    
    - Also, it doesn't allow to use document comment in single line comments now.
    
    Change-Id: I493476c803d8d7dd0935eb36d1bf6bc2d2ee968f

---
+++ MediaWiki/Sniffs/Commenting/IllegalSingleLineCommentSniff.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Detect and fix the inline comments start or end with multiple asterisks.
+ * Fail: /*** Comment here *\/
+ * Fail: /*** Comments here again ***\/
+ * Pass: /* Your comments here *\/
+ */
+ // @codingStandardsIgnoreStart
+class MediaWiki_Sniffs_Commenting_IllegalSingleLineCommentSniff
+	implements PHP_CodeSniffer_Sniff {
+	// @codingStandardsIgnoreEnd
+
+	/**
+	 * @return array
+	 */
+	public function register() {
+		return [
+			T_COMMENT
+		];
+	}
+
+	/**
+	 * @param  PHP_CodeSniffer_File $phpcsFile PHP_CodeSniffer_File object.
+	 * @param  int $stackPtr The index of current token.
+	 * @return void
+	 */
+	public function process( PHP_CodeSniffer_File $phpcsFile, $stackPtr ) {
+		$tokens = $phpcsFile->getTokens();
+		$currentToken = $tokens[$stackPtr];
+
+		if ( substr( $currentToken['content'], 0, 2 ) === '/*' ) {
+			// Possible inline comment
+			if ( strrpos( $currentToken['content'], '*/' ) === false ) {
+				// Whether it's a comment across multiple lines
+				$numOfTokens = count( $tokens );
+				for ( $i = $stackPtr + 1; $i < $numOfTokens; $i++ ) {
+					$token = $tokens[$i];
+					if ( strpos( $token['content'], '/*' ) === false &&
+						strrpos( $token['content'], '*/' )
+					) {
+						return;
+					}
+				}
+				$fix = $phpcsFile->addFixableError(
+					'Missing proper ending of a single line comment',
+					$stackPtr,
+					'MissingCommentEndding'
+				);
+				if ( $fix === true ) {
+					$phpcsFile->fixer->replaceToken(
+						$stackPtr,
+						str_replace( '\n', ' */', rtrim( $currentToken['content'] ) )
+					);
+				}
+			} else {
+				// Determine whether multiple "*" appears right after the "/*"
+				if ( preg_match( '/\/(\*){2,}/', $currentToken['content'] ) !== 0 ) {
+					$fix = $phpcsFile->addFixableWarning(
+						'Invalid start of a single line comment',
+						$stackPtr,
+						'IllegalSingleLineCommentStart'
+					);
+					if ( $fix === true ) {
+						$phpcsFile->fixer->replaceToken(
+							$stackPtr,
+							preg_replace( '/\/(\*){2,}/', '/*', $currentToken['content'] )
+						);
+					}
+				}
+				// Determine whether multiple "*" appears right before the "*/"
+				if ( preg_match( '/(\*){2,}\//', $currentToken['content'] ) !== 0 ) {
+					$fix = $phpcsFile->addFixableWarning(
+						'Invalid end of a single line comment',
+						$stackPtr,
+						'IllegalSingleLineCommentEnd'
+					);
+					if ( $fix === true ) {
+						$phpcsFile->fixer->replaceToken(
+							$stackPtr,
+							preg_replace( '/(\*){2,}\//', '*/', $currentToken['content'] )
+						);
+					}
+				}
+			}
+		}
+	}
+}
