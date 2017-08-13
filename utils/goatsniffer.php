1888411dbd98e83f3fce5ff260a6acb874df4020
Kunal Mehta <legoktm@member.fsf.org>
2017-08-13 13:42:59 -0400

    Add GoatSniffer ASCII art
    Change-Id: I10b688a156d8ffac81bd9ddcd07e2483425a484f

---
+++ utils/goatsniffer.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+/**
+ * Wikimania special edition: GoatSniffer
+ */
+echo <<<GOAT
+   _____             _    _____       _  __  __          
+  / ____|           | |  / ____|     (_)/ _|/ _|         
+ | |  __  ___   __ _| |_| (___  _ __  _| |_| |_ ___ _ __ 
+ | | |_ |/ _ \ / _` | __|\___ \| '_ \| |  _|  _/ _ \ '__|
+ | |__| | (_) | (_| | |_ ____) | | | | | | | ||  __/ |   
+  \_____|\___/ \__,_|\__|_____/|_| |_|_|_| |_| \___|_|   
+
+
+GOAT;
+
+// ASCII art by paroneayea
+echo <<<GOAT
+        /\
+       ' \\
+         .---
+         /    \_
+       // (~)  \
+ \\___//  _  _--  BAAAAA    BAAAAA
+  /      / \\\      BAAAAAA
+
+GOAT;
