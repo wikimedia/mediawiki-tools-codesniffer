fc7cb900fd5654beb49919e3a83977fb107aca7c
Kunal Mehta <legoktm@member.fsf.org>
2017-12-16 22:38:11 -0800

    Add sniff for using is_int over is_integer
    For consistency with our use of "int" everywhere instead of "integer".
    
    Conceptually this implementation is similar to the upstream
    ForbiddenFunctions sniff, however ours supports auto-fixing.
    
    Bug: T173175
    Change-Id: Ifbdb8267b5500a5ae77a6ea4d3000625ad81fbc4

---
+++ MediaWiki/Sniffs/Usage/ForbiddenFunctionsSniff.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Copyright (C) 2017 Kunal Mehta <legoktm@member.fsf.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+namespace MediaWiki\Sniffs\Usage;
+
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Sniffs\Sniff;
+
+class ForbiddenFunctionsSniff implements Sniff {
+
+	/**
+	 * Function => Replacement
+	 *
+	 * @var string[]
+	 */
+	private $functions = [
+		'is_integer' => 'is_int',
+	];
+
+	/**
+	 * @return array
+	 */
+	public function register() {
+		return [ T_STRING ];
+	}
+
+	/**
+	 * @param File $phpcsFile File object.
+	 * @param int $stackPtr The current token index.
+	 * @return void
+	 */
+	public function process( File $phpcsFile, $stackPtr ) {
+		$tokens = $phpcsFile->getTokens();
+
+		// Check if the function is one of the bad ones
+		$funcName = $tokens[$stackPtr]['content'];
+		if ( !isset( $this->functions[$funcName] ) ) {
+			return;
+		}
+
+		$ignore = [
+			T_DOUBLE_COLON => true,
+			T_OBJECT_OPERATOR => true,
+			T_FUNCTION => true,
+			T_CONST => true,
+		];
+
+		// Check to make sure it's a PHP function (not $this->, etc.)
+		$prevToken = $phpcsFile->findPrevious( T_WHITESPACE, ( $stackPtr - 1 ), null, true );
+		if ( isset( $ignore[$tokens[$prevToken]['code']] ) ) {
+			return;
+		}
+
+		$replacement = $this->functions[$funcName];
+		if ( $replacement ) {
+			$fix = $phpcsFile->addFixableWarning(
+				"Use $replacement() instead of $funcName",
+				$stackPtr,
+				$funcName
+			);
+			if ( $fix ) {
+				$phpcsFile->fixer->replaceToken( $stackPtr, $replacement );
+			}
+		} else {
+			$phpcsFile->addWarning(
+				"$funcName should not be used",
+				$stackPtr,
+				$funcName
+			);
+		}
+	}
+}
---
+++ MediaWiki/Tests/files/Usage/forbidden_functions_usage.php
@@ -0,0 +1,6 @@
+<?php
+
+is_int( 11 );
+is_integer( 12 );
+FooBar::is_integer( 13 );
+$this->is_integer( 14 );
