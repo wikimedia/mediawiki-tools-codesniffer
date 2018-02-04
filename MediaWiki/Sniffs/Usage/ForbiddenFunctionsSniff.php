311317543ceb8a6fa9d53f2a29226f89b5c1c3b7
Umherirrender <umherirrender_de.wp@web.de>
2018-02-04 19:40:33 +0100

    Forbid parse_str() without a second argument
    Bug: T183739
    Change-Id: I5a89cf4284b29c4d941b57483e8a362214d2474d

--- MediaWiki/Sniffs/Usage/ForbiddenFunctionsSniff.php
+++ MediaWiki/Sniffs/Usage/ForbiddenFunctionsSniff.php
@@ -22,6 +22,7 @@ namespace MediaWiki\Sniffs\Usage;
 
 use PHP_CodeSniffer\Files\File;
 use PHP_CodeSniffer\Sniffs\Sniff;
+use PHP_CodeSniffer\Util\Tokens;
 
 class ForbiddenFunctionsSniff implements Sniff {
 
@@ -41,6 +42,18 @@ class ForbiddenFunctionsSniff implements Sniff {
 		// Deprecated in PHP 7.2
 		'create_function' => false,
 		'each' => false,
+		'parse_str' => false,
+		'mb_parse_str' => false,
+	];
+
+	/**
+	 * Number of arguments to be forbidden
+	 *
+	 * @var int[]
+	 */
+	private $functionsArgCount = [
+		'parse_str' => 1,
+		'mb_parse_str' => 1,
 	];
 
 	/**
@@ -76,6 +89,19 @@ class ForbiddenFunctionsSniff implements Sniff {
 		if ( isset( $ignore[$tokens[$prevToken]['code']] ) ) {
 			return;
 		}
+		$nextToken = $phpcsFile->findNext( Tokens::$emptyTokens, $stackPtr + 1, null, true );
+		if ( $tokens[$nextToken]['code'] !== T_OPEN_PARENTHESIS ) {
+			return;
+		}
+
+		// Check argument count
+		if ( isset( $this->functionsArgCount[$funcName] ) ) {
+			$argCount = $this->argCount( $phpcsFile, $nextToken );
+			if ( $argCount !== $this->functionsArgCount[$funcName] ) {
+				// Nothing to replace
+				return;
+			}
+		}
 
 		$replacement = $this->functions[$funcName];
 		if ( $replacement ) {
@@ -88,11 +114,70 @@ class ForbiddenFunctionsSniff implements Sniff {
 				$phpcsFile->fixer->replaceToken( $stackPtr, $replacement );
 			}
 		} else {
-			$phpcsFile->addWarning(
-				"$funcName should not be used",
-				$stackPtr,
-				$funcName
-			);
+			if ( isset( $this->functionsArgCount[$funcName] ) ) {
+				$phpcsFile->addWarning(
+					"%s should not be used with %s argument(s)",
+					$stackPtr,
+					$funcName,
+					[ $funcName, $this->functionsArgCount[$funcName] ]
+				);
+			} else {
+				$phpcsFile->addWarning(
+					"$funcName should not be used",
+					$stackPtr,
+					$funcName
+				);
+			}
 		}
+	}
+
+	/**
+	 * Return the number of arguments between the $parenthesis as opener and its closer
+	 * Ignoring commas between brackets to support nested argument lists
+	 *
+	 * @param File $phpcsFile File object.
+	 * @param int $parenthesis The parenthesis token index.
+	 * @return int
+	 */
+	private function argCount( File $phpcsFile, $parenthesis ) {
+		$tokens = $phpcsFile->getTokens();
+		if ( !isset( $tokens[$parenthesis]['parenthesis_closer'] ) ) {
+			return 0;
+		}
+
+		$end = $tokens[$parenthesis]['parenthesis_closer'];
+		$next = $phpcsFile->findNext( Tokens::$emptyTokens, $parenthesis + 1, $end, true );
+		$argCount = 0;
+
+		if ( $next !== false ) {
+			// Something found, there is at least one argument
+			$argCount++;
+
+			$searchTokens = [
+				T_OPEN_CURLY_BRACKET,
+				T_OPEN_SQUARE_BRACKET,
+				T_OPEN_PARENTHESIS,
+				T_COMMA
+			];
+			while ( $next !== false ) {
+				switch ( $tokens[$next]['code'] ) {
+					case T_OPEN_CURLY_BRACKET:
+					case T_OPEN_SQUARE_BRACKET:
+					case T_OPEN_PARENTHESIS:
+						if ( isset( $tokens[$next]['parenthesis_closer'] ) ) {
+							// jump to closing parenthesis to ignore commas between opener and closer
+							$next = $tokens[$next]['parenthesis_closer'];
+						}
+						break;
+					case T_COMMA:
+						$argCount++;
+						break;
+				}
+
+				$next = $phpcsFile->findNext( $searchTokens, $next + 1, $end );
+			}
+		}
+
+		return $argCount;
 	}
 }
--- MediaWiki/Tests/files/Usage/forbidden_functions_usage.php
+++ MediaWiki/Tests/files/Usage/forbidden_functions_usage.php
@@ -5,3 +5,5 @@ is_integer( 12 );
 FooBar::is_integer( 13 );
 $this->is_integer( 14 );
 extract( $someArray );
+parse_str( 'arg' );
+parse_str( 'arg', $someArray['arrayIndex'] );
