7df8beb2b6e5b5c2fc9d087cb6eafcfef7a66b33
Umherirrender <umherirrender_de.wp@web.de>
2018-03-04 09:28:22 +0100

    Validate @license against SPDX
    Change-Id: Ie2dfe44e43bc60f65948d304317f7accba61f532

--- MediaWiki/Sniffs/Commenting/FunctionCommentSniff.php
+++ MediaWiki/Sniffs/Commenting/FunctionCommentSniff.php
@@ -13,7 +13,7 @@
  * @author Greg Sherwood <gsherwood@squiz.net>
  * @author Marc McIntyre <mmcintyre@squiz.net>
  * @copyright 2006-2014 Squiz Pty Ltd (ABN 77 084 670 600)
- * @license https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.txt BSD Licence
+ * @license https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.txt BSD-3-Clause
  * @link http://pear.php.net/package/PHP_CodeSniffer
  */
 
---
+++ MediaWiki/Tests/files/Commenting/license_comment.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @category Test
+ * @licence Unlicense
+ * @license BSD Clause
+ * @license
+ * @license https://spdx.org/licenses/GPL-2.0-or-later.html GPL-2.0-or-later
+ * @license https://spdx.org/licenses/GPL-2.0+.html GPL-2.0+
+ */
+class TestExamples {
+	/**
+	 * @license GPL-2.0-or-later
+	 */
+	public function noop() {
+	}
+}
