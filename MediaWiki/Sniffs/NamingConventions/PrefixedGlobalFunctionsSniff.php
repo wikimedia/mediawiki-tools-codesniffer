738d598be69d6a0f6f3dbfd71ec7371009673680
Umherirrender <umherirrender_de.wp@web.de>
2018-03-26 19:40:55 +0200

    Improve performance of PrefixedGlobalFunctionsSniff
    * Rearrange the check order. Begin with cheap global level check
    * Doing the expensive tokenIsNamespaced check at end
    * Improve the tokenIsNamespaced check by starting at top of file.
      When searching something first, it is better to search forward,
      then the search can stop after the success search.
      Also namespaces often at top of file
    * Simplify the cache by storing unreachable index instead of
      having a negative cache
    * The number of tokens is stored, no need for array_keys() + max().
      A count() seems also easier.
    
    Change-Id: I7f6cc727418fe4480ece9a93d3e448974608c95b

--- MediaWiki/Sniffs/NamingConventions/PrefixedGlobalFunctionsSniff.php
+++ MediaWiki/Sniffs/NamingConventions/PrefixedGlobalFunctionsSniff.php
@@ -27,11 +27,6 @@ class PrefixedGlobalFunctionsSniff implements Sniff {
 	private $firstNamespaceLocations = [];
 
 	/**
-	 * @var string[] array containing a list of files that contain no namespace statements.
-	 */
-	private $noNamespaceFiles = [];
-
-	/**
 	 * @param File $phpcsFile
 	 * @param int $ptr The current token index.
 	 *
@@ -39,49 +34,41 @@ class PrefixedGlobalFunctionsSniff implements Sniff {
 	 */
 	private function tokenIsNamespaced( File $phpcsFile, $ptr ) {
 		$fileName = $phpcsFile->getFilename();
-		// Check if we already know if the token is namespaced or not and return early if possible.
-		if (
-			isset( $this->firstNamespaceLocations[$fileName] ) &&
-			$ptr > $this->firstNamespaceLocations[$fileName]
-		) {
-			return true;
-		}
-		if ( isset( $this->noNamespaceFiles[$fileName] ) ) {
-			return false;
-		}
 
-		// If not scan the whole file at once looking for namespacing or lack of and set in the statics.
-		$tokens = $phpcsFile->getTokens();
-		$tokenIndex = max( array_keys( $tokens ) );
-		while ( $tokenIndex > 0 ) {
-			$token = $tokens[$tokenIndex];
-			if ( $token['code'] === T_NAMESPACE && !isset( $token['scope_opener'] ) ) {
-				// In the format of "namespace Foo;", which applies to everything below
-				$this->firstNamespaceLocations[$fileName] = $tokenIndex;
-			}
-			$tokenIndex--;
-		}
+		// Check if we already know if the token is namespaced or not
 		if ( !isset( $this->firstNamespaceLocations[$fileName] ) ) {
-			$this->noNamespaceFiles[$fileName] = true;
+			// If not scan the whole file at once looking for namespacing or lack of and set in the statics.
+			$tokens = $phpcsFile->getTokens();
+			$numTokens = $phpcsFile->numTokens;
+			for ( $tokenIndex = 0; $tokenIndex < $numTokens; $tokenIndex++ ) {
+				$token = $tokens[$tokenIndex];
+				if ( $token['code'] === T_NAMESPACE && !isset( $token['scope_opener'] ) ) {
+					// In the format of "namespace Foo;", which applies to everything below
+					$this->firstNamespaceLocations[$fileName] = $tokenIndex;
+					break;
+				}
+			}
+
+			// Nothing found, just save unreachable token index
+			if ( !isset( $this->firstNamespaceLocations[$fileName] ) ) {
+				$this->firstNamespaceLocations[$fileName] = $numTokens;
+			}
 		}
 
 		// Return if the token was namespaced.
-		if (
-			isset( $this->firstNamespaceLocations[$fileName] ) &&
-			$ptr > $this->firstNamespaceLocations[$fileName]
-		) {
-			return true;
-		}
-
-		return false;
+		return $ptr > $this->firstNamespaceLocations[$fileName];
 	}
+
 	/**
 	 * @param File $phpcsFile
 	 * @param int $stackPtr The current token index.
 	 * @return void
 	 */
 	public function process( File $phpcsFile, $stackPtr ) {
-		if ( $this->tokenIsNamespaced( $phpcsFile, $stackPtr ) ) {
+		$tokens = $phpcsFile->getTokens();
+
+		// Check if function is global
+		if ( $tokens[$stackPtr]['level'] !== 0 ) {
 			return;
 		}
 
@@ -90,21 +77,19 @@ class PrefixedGlobalFunctionsSniff implements Sniff {
 			return;
 		}
 
-		$tokens = $phpcsFile->getTokens();
-		$token = $tokens[$stackPtr];
-
-		// Check if function is global
-		if ( $token['level'] === 0 ) {
-			$prefix = substr( $name, 0, 2 );
-
-			if ( $prefix !== 'wf' && $prefix !== 'ef' ) {
-				// Forge a valid global function name
-				$expected = 'wf' . ucfirst( $name );
-
-				$error = 'Global function "%s" is lacking a \'wf\' prefix. Should be "%s".';
-				$data = [ $name, $expected ];
-				$phpcsFile->addError( $error, $stackPtr, 'wfPrefix', $data );
-			}
+		$prefix = substr( $name, 0, 2 );
+		if ( $prefix === 'wf' || $prefix === 'ef' ) {
+			return;
 		}
+
+		if ( $this->tokenIsNamespaced( $phpcsFile, $stackPtr ) ) {
+			return;
+		}
+
+		// Forge a valid global function name
+		$expected = 'wf' . ucfirst( $name );
+		$error = 'Global function "%s" is lacking a \'wf\' prefix. Should be "%s".';
+		$data = [ $name, $expected ];
+		$phpcsFile->addError( $error, $stackPtr, 'wfPrefix', $data );
 	}
 }
