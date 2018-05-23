bec403027434a03778b052b424bff6ac0eea03fa
Kunal Mehta <legoktm@member.fsf.org>
2018-05-23 03:51:20 -0700

    Prevent usage of nullable and void type hints
    These were introduced in PHP 7.1.
    
    Problematically, PHP 7.0 does not fail lint if a void return type is
    used. It also won't fail lint if a nullable parameter type hint is used.
    So use PHPCS to prevent those features from being introduced instead. It
    will fail lint if a nullable is used as a return type, but that was easy
    to implement so it is included for completeness.
    
    Change-Id: I54fa2a3f3904ca62f3a8cb070b650c871cbec8ec

---
+++ MediaWiki/Tests/files/PHP71Features/nullable_type_hint.php
@@ -0,0 +1,5 @@
+<?php
+
+function wfFoo( ?int $bar ): ?int {
+	return null;
+}
---
+++ MediaWiki/Tests/files/PHP71Features/void_return_type.php
@@ -0,0 +1,6 @@
+<?php
+
+// phpcs:ignore MediaWiki.PHP70Features.PHP70NewFeatures.T_RETURN_TYPE -- tested elsewhere
+function wfFoo(): void {
+	return;
+}
