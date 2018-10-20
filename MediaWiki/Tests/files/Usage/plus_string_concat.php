bc6e19957416fb8fb31d1f1ccc5ac8d6530e15ce
Umherirrender <umherirrender_de.wp@web.de>
2018-10-20 21:49:09 +0200

    Add sniff to detect + for string concat
    Bug: T195683
    Change-Id: I66abf9ebd7711f82edcebed9f0e3b02ca7f4349e

---
+++ MediaWiki/Tests/files/Usage/plus_string_concat.php
@@ -0,0 +1,14 @@
+<?php
+
+class PlusStringConcatSniffTest {
+
+	/**
+	 * @param string $a
+	 */
+	public function doing( $a ) {
+		$b = 'test' + 'test2';
+		$c = $b + $a;
+		$d = $c + 'string';
+	}
+
+}
