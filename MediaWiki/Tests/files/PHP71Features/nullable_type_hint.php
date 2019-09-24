8d70a0da7645e0b7692651b457429749c5cbc4d5
Max Semenik <maxsem.wiki@gmail.com>
2019-09-23 19:46:46 -0700

    Remove prohibitions on new PHP features
    Pending removal of HHVM support from MediaWiki.
    
    Bug: T231710
    Change-Id: I26fc676d7b8a9246d367c56552806d73fc59b620

--- MediaWiki/Tests/files/PHP71Features/nullable_type_hint.php
+++
@@ -1,5 +0,0 @@
-<?php
-// phpcs:disable MediaWiki.Commenting.FunctionComment
-function wfFoo( ?int $bar ): ?int {
-	return null;
-}
--- MediaWiki/Tests/files/PHP71Features/void_return_type.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-// phpcs:ignore MediaWiki.PHP70Features.PHP70NewFeatures.T_RETURN_TYPE -- tested elsewhere
-function wfFoo(): void {
-}
