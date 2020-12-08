fada1a2291ef1c27f72fad3cc0ba382c618eba10
DannyS712 <DannyS712.enwiki@gmail.com>
2020-12-08 23:26:02 +0000

    New sniff: MediaWiki.PHPUnit.AssertCount
    Warn when using assertEquals or assertSame to
    compare with the result of count(), and automatically
    replace with assertCount
    
    Bug: T242762
    Change-Id: If28fa6f1e2a67ac567257781e3e9bd796e071387

---
+++ MediaWiki/Tests/files/PHPUnit/assertions_count.php
@@ -0,0 +1,38 @@
+<?php
+
+// @phpcs:disable MediaWiki.PHPUnit.AssertEquals
+
+class AssertionsCountTest extends \PHPUnit\Framework\TestCase {
+
+	/**
+	 * @param int $a
+	 * @param int $b
+	 * @return int
+	 */
+	private function add( $a, $b = 0 ) {
+		return $a + $b;
+	}
+
+	/** @coversNothing */
+	public function testAssertions() {
+		$arr = [ 'a', 'b', 'c' ];
+		$c = 3;
+
+		// Simple cases
+		$this->assertEquals( 3, count( $arr ) );
+		$this->assertEquals( 3, count( $arr ), 'Message' );
+		$this->assertSame( 3, count( $arr ) );
+		$this->assertSame( 3, count( $arr ), 'Message' );
+
+		// More complex
+		$this->assertEquals( 1 + 2, count( $arr ) );
+		$this->assertSame( $this->add( 3 ), count( $arr ) );
+		$this->assertEquals( [ 0, 1, 2, 3 ][ 3 ], count( $arr ) );
+		$this->assertSame( $this->add( 1, 2 ), count( $arr ) );
+		$this->assertSame( $c, count( $arr ) );
+		$this->assertSame( $this->add( 1, count( [ 1, 2 ] ) ), count( $arr ) );
+
+		// Not replaced
+		$this->assertSame( 6, count( $arr ) * 2 );
+	}
+}
