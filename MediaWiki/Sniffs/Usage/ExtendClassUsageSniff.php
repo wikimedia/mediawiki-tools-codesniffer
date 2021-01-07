f0fff1fbd216e004cb57592850e4ab9dc0209feb
Ammarpad <ammarpad@yahoo.com>
2021-01-07 07:19:13 +0100

    Improve doc of ExtendClassUsageSniff
    Change-Id: Ia150cfc252edb0b2c68e231243958548df5c05c4

--- MediaWiki/Sniffs/Usage/ExtendClassUsageSniff.php
+++ MediaWiki/Sniffs/Usage/ExtendClassUsageSniff.php
@@ -1,6 +1,8 @@
 <?php
 /**
- * Report warnings when unexpected function or variable used like.
+ * Report warnings when a global variable or function is used where there's a better
+ * context-aware alternative.
+ *
  * Should use $this->msg() rather than wfMessage() on ContextSource extend.
  * Should use $this->getUser() rather than $wgUser on ContextSource extend.
  * Should use $this->getRequest() rather than $wgRequest on ContextSource extend.
@@ -19,8 +21,8 @@ class ExtendClassUsageSniff implements Sniff {
 	];
 
 	/**
-	 * Blacklist of globals, which cannot be used together with getConfig because there are objects,
-	 * not strings. There are excluded from reporting of this sniff.
+	 * List of globals which cannot be used together with getConfig() because most
+	 * of these are objects. They are excluded from reporting of this sniff.
 	 */
 	private const NON_CONFIG_GLOBALS_MEDIAWIKI_CORE = [
 		'$wgAuth',
@@ -43,7 +45,7 @@ class ExtendClassUsageSniff implements Sniff {
 	];
 
 	/**
-	 * Allow expand of the core blacklist for each extensions over .phpcs.xml by expand this setting.
+	 * Allow extensions add to the above list of non-config globals via .phpcs.xml
 	 * @var string[]
 	 */
 	public $nonConfigGlobals = [];
@@ -152,10 +154,10 @@ class ExtendClassUsageSniff implements Sniff {
 			return;
 		}
 
-		// Ignore namespace separator at the begin
+		// Ignore namespace separator at the beginning
 		$extClsContent = ltrim( $extClsContent, '\\' );
 
-		// Here should be replaced with a mechanism that check if
+		// This should be replaced with a mechanism that check if
 		// the base class is in the list of restricted classes
 		if ( !isset( self::CHECK_CONFIG['extendsCls'][$extClsContent] ) ) {
 			return;
@@ -198,8 +200,8 @@ class ExtendClassUsageSniff implements Sniff {
 				&& $i > $eligibleFunc['scope_start']
 				&& $i < $eligibleFunc['scope_end']
 			) {
-				// Inside a eligable function,
-				// check the current token against the checklist
+				// Inside eligible function, check the
+				// current token against the checklist
 				foreach ( $extClsCheckList as $value ) {
 					$condition = false;
 					if ( $value['code'] === T_FUNCTION
