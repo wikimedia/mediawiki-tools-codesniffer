1d17d6d935da1903b8d17d3af6cab1af426db045
Umherirrender <umherirrender_de.wp@web.de>
2021-03-19 23:51:14 +0100

    Test PrefixedGlobalFunctionsSniff with ignore list
    Change-Id: I846685c94fa64e5add4482ca02a377382fee7ac2

---
+++ MediaWiki/Tests/files/NamingConventions/wf_global_function_config.php
@@ -0,0 +1,19 @@
+<?php
+
+// phpcs:set MediaWiki.NamingConventions.PrefixedGlobalFunctions ignoreList[] myPassedExamples
+
+/**
+ * Failed examples.
+ * @return void
+ */
+function failedExamples() {
+	# code...
+}
+
+/**
+ * Passed examples.
+ * @return void
+ */
+function myPassedExamples() {
+	# code...
+}
