26e762bb7813c2ca450b39d384b3d691175c361a
Umherirrender <umherirrender_de.wp@web.de>
2021-03-20 12:32:21 +0100

    Test non-parenthesis and complex value branch in SpecificAssertionsSniff
    Change-Id: I794dbda2ed18dab1f8740111d44878a3c50925eb

--- MediaWiki/Tests/files/PHPUnit/specific_assertions.php
+++ MediaWiki/Tests/files/PHPUnit/specific_assertions.php
@@ -8,6 +8,7 @@ class SpecificAssertionsTest extends \PHPUnit\Framework\TestCase {
 	public function testAssertions() {
 		$arr = [ 'a', 'b', 'c' ];
 		$k = 'd';
+		$d = Foo::assertTrue;
 
 		// Replace
 		$this->assertTrue( array_key_exists( $k, $arr ) );
@@ -39,11 +40,13 @@ class SpecificAssertionsTest extends \PHPUnit\Framework\TestCase {
 		// Not replaced
 		$this->assertTrue( array_key_exists( $k, $arr ) || true );
 		$this->assertFalse( in_array( $k, $arr, true || true ) );
+		$this->assertFalse( in_array( $k + ( true ? [] : [ 'key' ] ) + [ 'value' ], $arr, 1 ) );
 		$this->assertFalse( strpos( 'foo', 'o', 1 ) );
 		$this->assertFalse( strpos( 'foo', 'o', 1 ), 'Message' );
 		$this->assertNotFalse( strpos( 'foo', 'o', 1 ) );
 		$this->assertNotFalse( strpos( 'foo', 'o', 1 ), 'Message' );
 		$this->assertIsInt( strpos( 'foo', 'o', 1 ) );
 		$this->assertIsInt( strpos( 'foo', 'o', 1 ), 'Message' );
+		$this->assertIsInt( strpos::intProperty, 'Message' );
 	}
 }
