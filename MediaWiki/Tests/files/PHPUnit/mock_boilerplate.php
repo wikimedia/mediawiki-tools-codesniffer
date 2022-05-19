d31ad788e8c174e055531ba7851d0c775c5a5942
DannyS712 <dannys712.wiki@gmail.com>
2022-05-19 01:08:41 +0000

    Add new MockBoilerplateSniff for PHPUnit tests
    Automatically simplifies setting up mock expectations
    and matchers.
    
    Change-Id: I05da4193e8d0169b4984107456139e97850d54bf

---
+++ MediaWiki/Tests/files/PHPUnit/mock_boilerplate.php
@@ -0,0 +1,58 @@
+<?php
+
+class MockBoilerplateTest extends \PHPUnit\Framework\TestCase {
+
+	/** @coversNothing */
+	public function testWillReplacement() {
+		$mock = $this->createMock( FooBar::class );
+		$mock->method( 'foo' )->will( $this->returnValue( 1 ) );
+		$mock->method( 'bar' )
+			->will( $this->returnValue( 2 ) );
+		$mock->method( 'arg1' )->will( $this->returnArgument( 0 ) );
+		$mock->method( 'ucfirst' )->will( $this->returnCallback( 'ucfirst' ) );
+		$mock->method( 'boolIsTrue' )->will( $this->returnValueMap( [
+			[ true, true ],
+			[ false, false ],
+		] ) );
+		$mock->method( 'self' )->will( $this->returnSelf() );
+		$mock->method( 'error' )->will( $this->throwException( $ex ) );
+	}
+
+	/** @coversNothing */
+	public function testExactlyShortcut() {
+		$mock = $this->createMock( FooBar::class );
+		$mock->expects( $this->exactly( 1 ) )->method( 'foo' )->willReturn( 1 );
+		$mock->expects( $this->exactly( 0 ) )
+			->method( 'bar' );
+	}
+
+	/** @coversNothing */
+	public function testWithEqualTo() {
+		$mock = $this->createMock( FooBar::class );
+		$mock->method( 'has' )->with( $this->equalTo( 'value' ) )->willReturn( true );
+		$mock->method( 'get' )->with( $this->equalTo( 'value' ) )->willReturn( 1 );
+		$mock->method( 'multi' )->with(
+			$this->equalTo( 'first' ),
+			'second',
+			$this->logicalOr( $this->equalTo( 'third-a' ), 'third-b' )
+		)->willReturn( false );
+		// equalTo() with a parameter that includes a comma (function call)
+		$mock->method( 'calculated' )->with(
+			$this->equalTo( addValues( 1, 2 ) )
+		)->willReturn( false );
+	}
+
+	/** @coversNothing */
+	public function testNotChanged() {
+		$mock = $this->createMock( FooBar::class );
+
+		$mock->expects( $this->exactly( 2 ) )->method( 'foo' );
+		$mock->expects( $this->once() )->method( 'bar' );
+
+		// equalTo() with a second parameter is skipped
+		$mock->method( 'baz' )->with(
+			$this->equalTo( 10, 0.1 )
+		)->willReturn( false );
+	}
+
+}
