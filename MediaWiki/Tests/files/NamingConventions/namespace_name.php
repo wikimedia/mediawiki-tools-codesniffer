21c4faa554cef9532a7055938a4788da2190000f
Umherirrender <umherirrender_de.wp@web.de>
2025-07-26 15:08:44 +0200

    Add new NamespaceNameSniff to check naming conventions
    Validate namespace names:
    - Not using wrong casing of "MediaWiki"
    - Not using "phpunit" namespace (mostly autogenerated)
    - Not using underscores (similiar to function and class names)
    - Not using lower camel case
    
    Bug: T397047
    Change-Id: I61ff4a26f5d4821ff2e45099edd63a3741b9e2f3

---
+++ MediaWiki/Tests/files/NamingConventions/namespace_name.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Mediawiki\Tests {
+
+}
+
+namespace phpunit\integration {
+
+}
+
+namespace MediaWiki\With_underscores {
+
+}
+
+namespace MediaWiki\notUpperCase {
+
+}
+
+namespace library\UpperCase {
+
+}
